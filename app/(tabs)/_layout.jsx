c22bd6a2124662e60e5732c89e98cd9924c9b39b
t1mane <146146478+t1mane@users.noreply.github.com>
2024-09-27 00:52:04 -0400

    fixed home page
    did changes on home and recent bar

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,13 +1,23 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function Tablayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name='home'/>
-      <Tabs.Screen name='explore'/>
-      <Tabs.Screen name='profile'/>
+    <Tabs screenOptions={{headerShown: false}}>
+      <Tabs.Screen name='home'
+      options={{
+        tabBarLabel: 'Home',
+        tabBarIcon:({color})=><FontAwesome name="home" size={24} color={color} />
+      }}
+      />
+      <Tabs.Screen name='Recent'options={{
+        tabBarLabel: 'Recent',
+        tabBarIcon:({color})=><MaterialIcons name="history" size={24} color={color} />
+      }}/>
+
 
     </Tabs>
   )
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,10 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="(tabs)" options={{
+        headerShown :false
+      }
+      } />
     </Stack>
   );
 }
