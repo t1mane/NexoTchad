9e15223e2ea344fe5d5c24f07c18ed9dd1efa335
t1mane <146146478+t1mane@users.noreply.github.com>
2024-09-27 01:17:35 -0400

    timane
    tabs

--- app/(tabs)/Recent.jsx
+++ app/(tabs)/Recent.jsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function profile() {
+export default function Recent() {
   return (
     <View>
       <Text>profile</Text>
--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function home(color='orange') {
+export default function home() {
   return (
     <View>
       <Text>home</Text>
