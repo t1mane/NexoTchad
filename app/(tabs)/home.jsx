62c8ec1610e95406246427b6495a661ff34327aa
t1mane <146146478+t1mane@users.noreply.github.com>
2024-09-27 18:22:27 -0400

    timane
    change some colors

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function home() {
   return (
     <View>
-      <Text>home</Text>
+      <Text style={{fontSize:40, fontfamily:'outfit'}}> home </Text>
     </View>
   )
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,10 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
+  useFonts({
+    'outfit' :require('./../assets/fonts/Roboto-Regular.ttf')
+  })
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{
