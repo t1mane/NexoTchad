43dd145b53d90c624f45711c47da2d5f05bb5a08
t1mane <146146478+t1mane@users.noreply.github.com>
2024-09-27 01:13:51 -0400

    Timane
    color change in tab bar

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -3,19 +3,20 @@ import React from 'react'
 import { Tabs } from 'expo-router'
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { Colors } from './../../constants/Colors'
 
 export default function Tablayout(color='white') {
   return (
-    <Tabs screenOptions={{headerShown: false}}>
+    <Tabs screenOptions={{headerShown: false, tabBarActiveTintColor:Colors.Primary }}>
       <Tabs.Screen name='home'
       options={{
         tabBarLabel: 'Home',
-        tabBarIcon:({color})=><FontAwesome name="home" size={24} color='black' />
+        tabBarIcon:({color})=><FontAwesome name="home" size={24} color={color} />
       }}
       />
       <Tabs.Screen name='Recent'options={{
         tabBarLabel: 'Recent',
-        tabBarIcon:({color})=><MaterialIcons name="history" size={24} color='black' />
+        tabBarIcon:({color})=><MaterialIcons name="history" size={24} color={color} />
       }}/>
 
 
