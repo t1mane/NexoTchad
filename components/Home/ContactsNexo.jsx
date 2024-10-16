659cc4cefdb4fb70fee8a09a76658a3858ac5de9
t1mane <146146478+t1mane@users.noreply.github.com>
2024-10-15 20:56:55 -0400

    timanr

--- components/Home/ContactsNexo.jsx
+++ components/Home/ContactsNexo.jsx
@@ -5,25 +5,25 @@ import { collection, query, where, getDocs, doc, getDoc, updateDoc, arrayUnion }
 
 export default function ContactsNexo() {
   const [modalVisible, setModalVisible] = useState(false);
+  const [contactModalVisible, setContactModalVisible] = useState(false);
+  const [selectedContact, setSelectedContact] = useState(null);
   const [name, setName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
-  const [contacts, setContacts] = useState([]); // Array to hold contacts
+  const [contacts, setContacts] = useState([]);
+  const [showAllContacts, setShowAllContacts] = useState(false);
 
-  // Fetch contacts from Firestore when the component mounts
   useEffect(() => {
     const fetchContacts = async () => {
       const currentUser = FIREBASE_AUTH.currentUser;
-
       if (!currentUser) return;
 
       try {
         const userRef = doc(FIRESTORE_DB, 'Users', currentUser.uid);
         const userDoc = await getDoc(userRef);
-
         if (userDoc.exists()) {
           const userData = userDoc.data();
-          setContacts(userData.contacts || []); // Set contacts or default to empty array
+          setContacts(userData.contacts || []);
         }
       } catch (error) {
         console.error('Error fetching contacts:', error);
@@ -32,7 +32,7 @@ export default function ContactsNexo() {
     };
 
     fetchContacts();
-  }, []); // Empty dependency array ensures this only runs on mount
+  }, []);
 
   const handleAddContact = () => {
     setModalVisible(true);
@@ -40,35 +40,32 @@ export default function ContactsNexo() {
 
   const handleSaveContact = async () => {
     const currentUser = FIREBASE_AUTH.currentUser;
-
     if (!currentUser) {
       Alert.alert('Error', 'User not authenticated');
       return;
     }
 
     try {
-      const normalizedEmail = email.trim().toLowerCase(); // Convert email to lowercase and trim whitespace
+      const normalizedEmail = email.trim().toLowerCase();
       const usersRef = collection(FIRESTORE_DB, 'Users');
       const q = query(usersRef, where('email', '==', normalizedEmail));
       const querySnapshot = await getDocs(q);
 
       if (!querySnapshot.empty) {
-        const contactDoc = querySnapshot.docs[0]; // Assuming one unique user per email
+        const contactDoc = querySnapshot.docs[0];
         const contactData = contactDoc.data();
         const contactId = contactDoc.id;
 
-        // Add the contact to the current user's contacts array
         const userRef = doc(FIRESTORE_DB, 'Users', currentUser.uid);
         await updateDoc(userRef, {
           contacts: arrayUnion({
             contactId,
             name,
             lastName,
-            email: contactData.email, // Store email in consistent lowercase form
+            email: contactData.email,
           }),
         });
 
-        // Update the local contacts state to reflect the new contact
         setContacts((prevContacts) => [
           ...prevContacts,
           { name, lastName, email: contactData.email },
@@ -85,6 +82,11 @@ export default function ContactsNexo() {
     }
   };
 
+  const handleContactPress = (contact) => {
+    setSelectedContact(contact);
+    setContactModalVisible(true);
+  };
+
   const closeModal = () => {
     setModalVisible(false);
     setName('');
@@ -92,30 +94,35 @@ export default function ContactsNexo() {
     setEmail('');
   };
 
+  const closeContactModal = () => {
+    setContactModalVisible(false);
+    setSelectedContact(null);
+  };
+
+  const visibleContacts = showAllContacts ? contacts : contacts.slice(0, 9);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Contacts</Text>
       
       <View style={styles.contactsBox}>
-        <View style={styles.contactRow}>
-          {contacts.map((contact, index) => (
-            <View key={index} style={styles.contactCircle}>
-              <Text style={styles.contactText}>{contact.name[0]}{contact.lastName[0]}</Text>
-            </View>
-          ))}
-          <TouchableOpacity style={styles.addContactCircle} onPress={handleAddContact}>
-            <Text style={styles.addText}>+</Text>
+        {visibleContacts.map((contact, index) => (
+          <TouchableOpacity key={index} style={styles.contactCircle} onPress={() => handleContactPress(contact)}>
+            <Text style={styles.contactText}>{contact.name[0]}{contact.lastName[0]}</Text>
           </TouchableOpacity>
-        </View>
+        ))}
+        {contacts.length > 9 && !showAllContacts && (
+          <TouchableOpacity style={styles.dropdownButton} onPress={() => setShowAllContacts(true)}>
+            <Text style={styles.dropdownButtonText}>Voir tous les contacts ▼</Text>
+          </TouchableOpacity>
+        )}
+        <TouchableOpacity style={styles.addContactCircle} onPress={handleAddContact}>
+          <Text style={styles.addText}>+</Text>
+        </TouchableOpacity>
       </View>
 
       {/* Modal for Creating Contact */}
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={closeModal}
-      >
+      <Modal animationType="slide" transparent={true} visible={modalVisible} onRequestClose={closeModal}>
         <TouchableOpacity style={styles.modalOverlay} activeOpacity={1} onPress={closeModal}>
           <TouchableOpacity style={styles.modalContainer} activeOpacity={1} onPress={() => {}}>
             <Text style={styles.modalTitle}>Créer un Contact</Text>
@@ -153,100 +160,53 @@ export default function ContactsNexo() {
           </TouchableOpacity>
         </TouchableOpacity>
       </Modal>
+
+      {/* Modal for Contact Details */}
+      <Modal animationType="slide" transparent={true} visible={contactModalVisible} onRequestClose={closeContactModal}>
+        <TouchableOpacity style={styles.modalOverlay} activeOpacity={1} onPress={closeContactModal}>
+          <TouchableOpacity style={styles.modalContainer} activeOpacity={1} onPress={() => {}}>
+            {selectedContact && (
+              <>
+                <Text style={styles.modalTitle}>Détails du Contact</Text>
+                <Text style={styles.contactDetailsText}>Nom: {selectedContact.name}</Text>
+                <Text style={styles.contactDetailsText}>Prénom: {selectedContact.lastName}</Text>
+                <Text style={styles.contactDetailsText}>Email: {selectedContact.email}</Text>
+
+                {/* Transfer and Request Funds Buttons */}
+                <View style={styles.buttonContainer}>
+                  <TouchableOpacity style={styles.button} onPress={() => console.log('Transfer Funds')}>
+                    <Text style={styles.buttonText}>Transférer des fonds</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.button} onPress={() => console.log('Request Funds')}>
+                    <Text style={styles.buttonText}>Demander des fonds</Text>
+                  </TouchableOpacity>
+                </View>
+              </>
+            )}
+          </TouchableOpacity>
+        </TouchableOpacity>
+      </Modal>
     </View>
   );
 }
 
-
 const styles = StyleSheet.create({
-  container: {
-    padding: 8,
-    alignItems: 'flex-start',
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 10,
-    fontFamily: 'Oswald-Bold',
-  },
-  contactsBox: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 10,
-    padding: 10,
-    alignItems: 'center',
-    width: '100%',
-  },
-  contactRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 10,
-  },
-  contactCircle: {
-    width: 60,           // Larger width for contact circles
-    height: 60,          // Larger height for contact circles
-    borderRadius: 30,    // Half of the width/height for a circular shape
-    backgroundColor: '#ddd',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginHorizontal: 10,
-  },
-  addContactCircle: {
-    width: 60,
-    height: 60,
-    borderRadius: 20,
-    backgroundColor: '#4caf50',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginHorizontal: 10,
-  },
-  addText: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#fff',
-    fontFamily:"oswald"
-  },
-  modalOverlay: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  modalContainer: {
-    width: 300,
-    padding: 20,
-    backgroundColor: 'white',
-    borderRadius: 10,
-    alignItems: 'center',
-  },
-  modalTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 10,
-    fontFamily:"oswald"
-  },
-  input: {
-    width: '100%',
-    height: 40,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    borderRadius: 5,
-    paddingHorizontal: 10,
-    marginBottom: 10,
-    color: '#000', // Ensures text within input fields is black
-  },
-  saveButton: {
-    backgroundColor: '#ff5a00',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-    marginTop: 10,
-  },
-  saveButtonText: {
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    fontFamily:'oswald'
-  },
+  container: { padding: 8, alignItems: 'flex-start' },
+  title: { fontSize: 18, fontWeight: 'bold', marginBottom: 10, fontFamily: 'Oswald-Bold' },
+  contactsBox: { borderWidth: 1, borderColor: '#ccc', borderRadius: 10, padding: 10, alignItems: 'center', width: '100%' },
+  contactCircle: { width: 60, height: 60, borderRadius: 30, backgroundColor: '#ddd', justifyContent: 'center', alignItems: 'center', marginHorizontal: 10, marginBottom: 10 },
+  addContactCircle: { width: 60, height: 60, borderRadius: 30, backgroundColor: '#4caf50', justifyContent: 'center', alignItems: 'center', marginHorizontal: 10, marginBottom: 10 },
+  addText: { fontSize: 20, fontWeight: 'bold', color: '#fff', fontFamily: "oswald" },
+  dropdownButton: { marginTop: 10, paddingVertical: 5, paddingHorizontal: 10, backgroundColor: '#ddd', borderRadius: 5 },
+  dropdownButtonText: { color: '#555', fontWeight: 'bold', textAlign: 'center', fontFamily: 'Oswald-Bold' },
+  modalOverlay: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)' },
+  modalContainer: { width: 300, padding: 20, backgroundColor: 'white', borderRadius: 10, alignItems: 'center' },
+  modalTitle: { fontSize: 18, fontWeight: 'bold', marginBottom: 10, fontFamily: "oswald" },
+  input: { width: '100%', height: 40, borderColor: '#ccc', borderWidth: 1, borderRadius: 5, paddingHorizontal: 10, marginBottom: 10, color: '#000' },
+  saveButton: { backgroundColor: '#ff5a00', paddingVertical: 10, paddingHorizontal: 20, borderRadius: 5, marginTop: 10 },
+  saveButtonText: { color: '#fff', fontSize: 16, fontWeight: 'bold', textAlign: 'center', fontFamily: 'oswald' },
+  contactDetailsText: { fontSize: 16, marginVertical: 5, fontFamily: 'oswald' },
+  buttonContainer: { flexDirection: 'row', justifyContent: 'space-between', marginTop: 20 },
+  button: { flex: 1, backgroundColor: '#ff5a00', paddingVertical: 10, marginHorizontal: 5, borderRadius: 5, alignItems: 'center' },
+  buttonText: { color: '#fff', fontSize: 15, fontFamily: 'Oswald' }
 });
